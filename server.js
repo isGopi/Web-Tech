14ee2e5e8849b2dc6af3d72cc1bdd23cd460b8b2
Gopi <73015341+isGopi@users.noreply.github.com>
2025-02-12 00:09:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 // Serve static files from the "web" directory
 app.use(express.static(path.join(__dirname, 'web')));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// Use the port provided by Heroku or default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
